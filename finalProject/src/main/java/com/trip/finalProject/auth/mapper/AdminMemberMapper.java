d9fd8b48c583bb48dee7e538db64fae6a1248db8
be_jadely <p24fzgfktr@privaterelay.appleid.com>
2023-08-09 23:27:59 +0900

    관리자 마이페이지 view layout 1차 설계 완료 및 관리자 권한 승인 트랜젝션 완성
    - 관리자 마이페이지용 thymeleaf layout을 재설계하였습니다.
    - 관리자 권한승인 시 member.authority 상태 변경 및 auth_confirm 테이블에 데이터 추가가
      하나의 트랜젝션으로 실행가능하도록 구현 완료 하였습니다.

---
+++ finalProject/src/main/java/com/trip/finalProject/auth/mapper/AdminMemberMapper.java
@@ -0,0 +1,15 @@
+package com.trip.finalProject.auth.mapper;
+
+import java.util.List;
+
+import com.trip.finalProject.auth.service.AdminMemberVO;
+
+public interface AdminMemberMapper {
+	
+	// 권한 승인 신청 회원 전체 조회
+	public List<AdminMemberVO> selectAllAuthRequest();
+	
+	// 권한 승인 요청 승인 처리
+	public int approveAuthRequest(String memberId);
+	
+}
--- finalProject/src/main/java/com/trip/finalProject/auth/service/AdminMemberService.java
+++ finalProject/src/main/java/com/trip/finalProject/auth/service/AdminMemberService.java
@@ -1,6 +1,7 @@
-package com.trip.finalProject.admin.service;
+package com.trip.finalProject.auth.service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface AdminMemberService {
 	
@@ -8,5 +9,8 @@ public interface AdminMemberService {
 	
 	// 권한 승인 요청 회원 전체 조회
 	public List<AdminMemberVO> selectAllAuthRequest();
+	
+	// 권한 승인 처리
+	public Map<String, String> approveAuthRequest(String memberId);
 	
 }
