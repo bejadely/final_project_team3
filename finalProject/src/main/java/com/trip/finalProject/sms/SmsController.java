e4d11bfa0d010f77c150ff843358ab85edd99286
2eek <khwan9404@yu.ac.kr>
2023-08-14 19:46:09 +0900

    sms 직접 메세지 입력 전송 완

---
+++ finalProject/src/main/java/com/trip/finalProject/sms/SmsController.java
@@ -0,0 +1,37 @@
+package com.trip.finalProject.sms;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.client.RestClientException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import lombok.RequiredArgsConstructor;
+ 
+@Controller
+@RequiredArgsConstructor
+public class SmsController {
+	
+	private final SmsService smsService;
+	
+	@GetMapping("/send")
+	public String getSmsPage() {
+		return "sms/sendSms";
+	}
+	
+	@PostMapping("/sms/send")
+	public String sendSms(MessageDTO messageDto, Model model) throws JsonProcessingException, RestClientException, URISyntaxException, InvalidKeyException, NoSuchAlgorithmException, UnsupportedEncodingException {
+		SmsResponseDTO response = smsService.sendSms(messageDto);
+		model.addAttribute("response", response);
+		return "result";
+	}
+ 
+	
+}
---
+++ finalProject/src/main/java/com/trip/finalProject/sms/SmsRequestDTO.java
@@ -0,0 +1,24 @@
+package com.trip.finalProject.sms;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class SmsRequestDTO {
+	String type;
+	String contentType;
+	String countryCode;
+	String from;
+	String content;
+	List<MessageDTO> messages;
+	
+}
