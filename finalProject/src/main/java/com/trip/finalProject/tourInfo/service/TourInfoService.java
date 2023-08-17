1b926cc4d4b15a324b24aea87322c4eb14b881e9
admin <admin@YD02-05>
2023-08-17 17:04:45 +0900

    0817 yuri 관광정보페이지

--- finalProject/src/main/java/com/trip/finalProject/tourInfo/service/TourInfoService.java
+++ finalProject/src/main/java/com/trip/finalProject/tourInfo/service/TourInfoService.java
@@ -1,7 +1,10 @@
 package com.trip.finalProject.tourInfo.service;
 
+import java.util.List;
 import java.util.Map;
 
 public interface TourInfoService {
-    Map<String, Object> getTourInfoMap();
+    Map<String, Object> getTourInfoMap(int areaCode, int sigunguCode);
+
+    Map<String, String> getDetailInfoMap(int contentId, int contentTypeId);
 }
