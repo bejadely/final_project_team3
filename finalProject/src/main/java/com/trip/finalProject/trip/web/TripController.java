fe45368fe189f52fe1da7fccbc3a49509c77ae23
admin <admin@YD02-18>
2023-08-18 17:57:02 +0900

    키워드 검색 후 해당 리스트 클릭시 커서 생성 완료, 선으로 연결하는 작업중

--- finalProject/src/main/java/com/trip/finalProject/trip/web/TripController.java
+++ finalProject/src/main/java/com/trip/finalProject/trip/web/TripController.java
@@ -18,67 +18,66 @@ import com.trip.finalProject.trip.service.TripVO;
 public class TripController {
 	@Autowired
 	TripService tripService;
-	
+
 	@Value("${kakao.map.key}")
 	String kakaoMap;
-	
-	//여행기록 전체 조회
+
+	// 여행기록 전체 조회
 	@GetMapping("tripRecordList")
-	public String tirpRecordList(Model model
-			  ,@RequestParam(value = "nowPage", defaultValue = "1") Integer nowPage
-			  ,@RequestParam(value = "cntPerPage", defaultValue = "12") Integer cntPerPage) {
+	public String tirpRecordList(Model model, @RequestParam(value = "nowPage", defaultValue = "1") Integer nowPage,
+			@RequestParam(value = "cntPerPage", defaultValue = "12") Integer cntPerPage) {
 		int total = tripService.tripRecordCount();
 		PagingVO pagingVO = new PagingVO(total, nowPage, cntPerPage);
 		List<TripVO> tripRecordList = tripService.getTripAll(pagingVO);
-		
+
 		model.addAttribute("tripRecordList", tripRecordList);
 		model.addAttribute("paging", pagingVO);
-		
+
 		return "trip/tripRecordList";
 	}
-	
-	//여행기록 상세조회
+
+	// 여행기록 상세조회
 	@GetMapping("tripRecordInfo")
 	public String tripRecordInfo(TripVO tripVO, Model model) {
 		TripVO findVO = tripService.getTripInfo(tripVO);
 		model.addAttribute("tripInfo", findVO);
 		return "trip/tripRecordInfo";
 	}
-	
-	//여행기록 등록 - form
+
+	// 여행기록 등록 - form
 	@PostMapping("tripRecordInsertForm")
 	public String tripRecordInsertForm(TripVO tripVO, Model model) {
 		model.addAttribute("tripVO", tripVO);
 		return "trip/tripRecordInsertForm";
 	}
-	
-	//여행기록 등록 - 처리
+
+	// 여행기록 등록 - 처리
 	@PostMapping("tripRecordInsert")
 	public String tripRecordInsertProcess(TripVO tripVO) {
 		tripService.InsertTripInfo(tripVO);
 		return "redirect:/tripRecordList";
 	}
-	
-	//여행기록 임시저장 - 처리
+
+	// 여행기록 임시저장 - 처리
 	@PostMapping("tsTripRecordInsert")
 	public String tsTripRecordInsertProcess(TripVO tripVO) {
 		tripService.TsInsertTripInfo(tripVO);
 		return "redirect:/tripRecordList";
 	}
-	
-	
-	//여행기록 지도 맵핑 - form
-	@GetMapping("tripMappingForm")
-	public String tripMappingForm(){
-		return "trip/tripMappingForm";
+
+	// 여행기록 지도 맵핑 - form
+	@GetMapping("tripMappingInsertForm")
+	public String tripMappingInsertForm(TripVO tripVO, Model model) {
+		model.addAttribute("tripVO", tripVO);
+		return "trip/tripMappingInsertForm";
 	}
-	
-	//여행기록 지도 맵핑 - 처리
+
+	// 여행기록 지도 맵핑 - 처리
 	@PostMapping("tripMappingInsert")
 	public String tripMappingInsertProcess(TripVO tripVO) {
 		tripService.TsInsertTripInfo(tripVO);
 		return "redirect:/tripInsertForm";
 	}
-	
-	//여행기록 메모 등록
+
+	// 여행기록 메모 등록
 }
