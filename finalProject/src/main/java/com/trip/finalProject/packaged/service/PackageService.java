10691cde746a7ca4fc57ffe9f66e6110e3eeab7a
admin <admin@YD02-20>
2023-08-18 12:48:12 +0900

    집에서 하자

---
+++ finalProject/src/main/java/com/trip/finalProject/packaged/service/PackageService.java
@@ -0,0 +1,15 @@
+package com.trip.finalProject.packaged.service;
+
+import java.util.List;
+
+public interface PackageService {
+	
+	//패키지 등록
+	public int insertEdirotInfo(PackageVO packageVO);
+	
+	//패키지 정보
+	public PackageVO packageInfo(PackageVO packageVO);
+	
+	//패키지 리스트
+	public List<PackageVO> getPackageList();
+}
