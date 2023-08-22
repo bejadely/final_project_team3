737830e23f927bac4ba1826071ec6a4d29650f36
admin <admin@YD02-05>
2023-08-22 19:30:31 +0900

    관광정보페이지 slick.js 구현 완료, 관광정보페이지 검색창 2/3

---
+++ finalProject/src/main/java/com/trip/finalProject/festival/service/FestivalInfoVO.java
@@ -0,0 +1,22 @@
+package com.trip.finalProject.festival.service;
+
+
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class FestivalInfoVO {
+	private String festivalId;
+	private String areaCode;
+	private String sigunguCode;
+	private String festivalName;
+	private String content;
+	private String festivalUrl;
+	private String startDate;
+	private String endDate;
+	private String festivalImg;
+	private String contentId;
+	private Date saveDate;
+}
---
+++ finalProject/src/main/java/com/trip/finalProject/tourInfo/service/LocalTourInfoDTO.java
@@ -0,0 +1,14 @@
+package com.trip.finalProject.tourInfo.service;
+
+import lombok.Data;
+
+@Data
+public class LocalTourInfoDTO {
+	
+	private String contentId;
+	private String contentTypeId;
+	private String firstImage;
+	private String title;
+	private String address;
+	
+}
