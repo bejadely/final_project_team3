00e6ded68cdec32d01e2b57178fb9e8858450af0
2eek1 <khwan9404@yu.ac.kr>
2023-08-23 17:41:24 +0900

    공지사항 매퍼 개발중

--- finalProject/src/main/java/com/trip/finalProject/notice/mapper/NoticeMapper.java
+++ finalProject/src/main/java/com/trip/finalProject/notice/mapper/NoticeMapper.java
@@ -1,10 +1,31 @@
 package com.trip.finalProject.notice.mapper;
 
 
+import java.util.List;
+
 import com.trip.finalProject.notice.service.NoticeVO;
 
 public interface NoticeMapper {
 	
 	public NoticeVO insertpost(NoticeVO noticeVO);
 
+	public List<NoticeVO> boardSelectList(NoticeVO vo) ;
+	
+	public List<NoticeVO> boardSelect(NoticeVO vo);
+	
+	public int boardInsert(NoticeVO vo);
+	
+	public int boardUpdate(NoticeVO vo);
+	
+	public int boardDelete(NoticeVO vo);
+	
+	public int boardSelectMax(NoticeVO vo);
+	
+	public NoticeVO boardDetail(NoticeVO vo);
+	
+	public int boardView(NoticeVO vo);
+	
+	public int boardReple(NoticeVO vo);
+	
+	public int boardRepleN(NoticeVO vo);
 }
