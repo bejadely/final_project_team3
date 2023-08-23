fcf76634503741436563a278f27232254e198238
2eek1 <khwan9404@yu.ac.kr>
2023-08-23 12:26:41 +0900

    회원가입 완료. 공지사항 개발 진행중

---
+++ finalProject/src/main/java/com/trip/finalProject/notice/mapper/NoticeMapper.java
@@ -0,0 +1,10 @@
+package com.trip.finalProject.notice.mapper;
+
+
+import com.trip.finalProject.notice.service.NoticeVO;
+
+public interface NoticeMapper {
+	
+	public NoticeVO insertpost(NoticeVO noticeVO);
+
+}
