b75520dfc28ef22db8893bfb3c17bbd5e4c97635
admin <admin@YD02-20>
2023-08-23 14:47:06 +0900

    패키지 정보 DB에 등록 구현중

--- finalProject/src/main/java/com/trip/finalProject/packaged/service/PackageVO.java
+++ finalProject/src/main/java/com/trip/finalProject/packaged/service/PackageVO.java
@@ -1,9 +1,24 @@
 package com.trip.finalProject.packaged.service;
 
+import java.util.Date;
+import java.util.List;
+
+import com.trip.finalProject.attachedFile.service.AttachedFileVO;
+
 import lombok.Data;
 
 @Data
 public class PackageVO {
 	private String postId;
 	private String content;
+	private String mainImage;
+	private String name;
+	private int price;
+	private Date startDate;
+	private Date endDate;
+	private Date deadlineDate;
+	private int maxReservation;
+	private String tourTheme;
+	
+	private List<AttachedFileVO> attachList;
 }
