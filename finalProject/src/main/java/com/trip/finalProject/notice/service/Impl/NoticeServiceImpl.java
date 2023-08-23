6802180e8359c7a870af55e8d7d83e359ac25298
2eek1 <khwan9404@yu.ac.kr>
2023-08-23 16:34:33 +0900

    공지사항 개발중

--- finalProject/src/main/java/com/trip/finalProject/notice/service/Impl/NoticeServiceImpl.java
+++ finalProject/src/main/java/com/trip/finalProject/notice/service/Impl/NoticeServiceImpl.java
@@ -2,12 +2,15 @@ package com.trip.finalProject.notice.service.Impl;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.trip.finalProject.notice.mapper.NoticeMapper;
 import com.trip.finalProject.notice.service.NoticeService;
 import com.trip.finalProject.notice.service.NoticeVO;
+
 
 @Service
 public class NoticeServiceImpl implements NoticeService {
@@ -29,4 +32,68 @@ public class NoticeServiceImpl implements NoticeService {
 		}
 
     }
+    
+    @Override
+	public List<NoticeVO> boardSelectList(NoticeVO vo) {
+
+		return noticeMapper.boardSelectList(vo);
+	}
+
+	@Override
+	public List<NoticeVO> NoticeVO(NoticeVO vo) {
+	
+		return noticeMapper.boardSelect(vo);
+	}
+
+	@Override
+	public int boardInsert(NoticeVO vo) {
+
+		return noticeMapper.boardInsert(vo);
+	}
+
+	@Override
+	public int boardUpdate(NoticeVO vo) {
+		
+		return noticeMapper.boardUpdate(vo);
+	}
+
+	@Override
+	public int boardDelete(NoticeVO vo) {
+
+		return noticeMapper.boardDelete(vo);
+	}
+
+
+
+	@Override
+	public int boardSelectMax(NoticeVO vo) {
+
+		return noticeMapper.boardSelectMax(vo);
+	}
+
+	@Override
+	public NoticeVO boardDetail(NoticeVO vo) {
+	
+		return noticeMapper.boardDetail(vo);
+	}
+
+	@Override
+	public int boardView(NoticeVO vo) {
+
+		return noticeMapper.boardView(vo);
+	}
+
+	@Override
+	public int boardReple(NoticeVO vo) {
+	
+		return noticeMapper.boardReple(vo);
+	}
+
+	@Override
+	public int boardRepleN(NoticeVO vo) {
+	
+		return noticeMapper.boardRepleN(vo);
+	}
+    
+    
 }
--- finalProject/src/main/java/com/trip/finalProject/notice/web/NoticeController.java
+++ finalProject/src/main/java/com/trip/finalProject/notice/web/NoticeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import com.trip.finalProject.notice.service.NoticeService;
 import com.trip.finalProject.notice.service.NoticeVO;
 
@@ -14,6 +15,49 @@ public class NoticeController {
 	private NoticeService noticeService;
 
    
+	@GetMapping("/boardList")
+	public String selectBoardList() {
+		return"notice/BoardList";
+	};
+	@GetMapping("/boardSearch")
+	public String boardSearch() {
+		return"notice/BoardList";
+	};
+	@GetMapping("/boardWrite")
+	public String boardWrite() {
+		return"notice/BoardList";
+	};
+	@GetMapping("/boardInsert")
+	public String boardInsert() {
+		return"notice/BoardList";
+	};
+	@GetMapping("/boardSelect")
+	public String boardSelect() {
+		return"notice/BoardList";
+	};
+	@GetMapping("/boardEdit")
+	public String boardEdit() {
+		return"notice/BoardList";
+	};
+	@GetMapping("/boardUpdate")
+	public String boardUpdate() {
+		return"notice/BoardList";
+	};
+	@GetMapping("/boardDelete")
+	public String boardDelete() {
+		return"notice/BoardList";
+	};
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	@GetMapping("/notice") 
 	public String memberInsertForm() {
 		return"notice/noticeBoard";
@@ -26,9 +70,12 @@ public class NoticeController {
 		//Service를 호출하여 insertPmtNtmForm() 실행
 		noticeService.insertpost(noticeVO);
 		 // 공지사항 등록 후 리스트 화면으로 이동
-		
-		
+			
 		// view에 결과 넘김
 		return "redirect:/";
 	}
+	
+	
+	
+	
 }
