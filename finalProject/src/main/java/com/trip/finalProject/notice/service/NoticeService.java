5c6e2ee59423b6f53e13b96b2eb98e35928113a3
2eek1 <khwan9404@yu.ac.kr>
2023-08-23 16:34:33 +0900

    공지사항 개발중

--- finalProject/src/main/java/com/trip/finalProject/notice/service/NoticeService.java
+++ finalProject/src/main/java/com/trip/finalProject/notice/service/NoticeService.java
@@ -1,10 +1,20 @@
 package com.trip.finalProject.notice.service;
 
+import java.util.List;
 
 
 public interface NoticeService {
 	
 	
 	public NoticeVO insertpost(NoticeVO noticeVO) throws Exception; 
-
+	public List<NoticeVO> boardSelectList(NoticeVO vo);
+	public List<NoticeVO> NoticeVO(NoticeVO vo);
+	public int boardInsert(NoticeVO vo);
+	public int boardUpdate(NoticeVO vo);
+	public int boardDelete(NoticeVO vo);
+	public int boardSelectMax(NoticeVO vo);
+	public NoticeVO boardDetail(NoticeVO vo);
+	public int boardView(NoticeVO vo);
+	public int boardReple(NoticeVO vo);
+	public int boardRepleN(NoticeVO vo);
 }
