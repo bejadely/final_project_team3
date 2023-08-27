3876f998dd6859410f6537d6157d945be493b8ce
2eek <khwan9404@yu.ac.kr>
2023-08-27 18:21:30 +0900

    공지사항 등록 조회 완료

--- finalProject/src/main/java/com/trip/finalProject/notice/service/NoticeService.java
+++ finalProject/src/main/java/com/trip/finalProject/notice/service/NoticeService.java
@@ -2,13 +2,17 @@ package com.trip.finalProject.notice.service;
 
 import java.util.List;
 
+import com.trip.finalProject.common.PagingVO;
+
 
 public interface NoticeService {
 	
 	
 	public NoticeVO insertpost(NoticeVO noticeVO) throws Exception; 
-	public List<NoticeVO> boardSelectList(NoticeVO vo);
-	public List<NoticeVO> boardSelect(NoticeVO vo);
+	
+	
+	//public List<NoticeVO> boardSelect(NoticeVO vo);
+	//공지사항 등록
 	public int noticeInsert(NoticeVO vo);
 	public int boardUpdate(NoticeVO vo);
 	public int boardDelete(NoticeVO vo);
@@ -17,4 +21,8 @@ public interface NoticeService {
 	public int boardView(NoticeVO vo);
 	public int boardReple(NoticeVO vo);
 	public int boardRepleN(NoticeVO vo);
+	//공지사항 화면에 뿌리기
+	public List<NoticeVO> SelectAllNoticeList(PagingVO pagingVO);
+	//게시글 수 카운트
+	public int listCount();
 }
