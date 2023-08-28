cf4e1b1b795441614cf9f08aa2d400480ffc9f56
admin <admin@YD02-18>
2023-08-28 14:00:18 +0900

    0828 merge전 커밋 - 에디터를 통한 메이트 등록글 등록 구현중

--- finalProject/src/main/java/com/trip/finalProject/tripMate/mapper/TripMateMapper.java
+++ finalProject/src/main/java/com/trip/finalProject/tripMate/mapper/TripMateMapper.java
@@ -11,6 +11,9 @@ public interface TripMateMapper {
 	//여행 메이트 글 상세조회
 	public TripMateVO selectTripMateInfo(TripMateVO tripMateVO);
 	
+	//여행 메이트 게시글 등록
+	public int insertEditor(TripMateVO tripMateVO);
+	
 	//여행 메이트 신청 (등록된 게시글에 대한 여행메이트 신청)
 	public int insertTripMateApply(TripMateVO tripMateVO); 
 }
--- finalProject/src/main/java/com/trip/finalProject/tripMate/web/TripMateController.java
+++ finalProject/src/main/java/com/trip/finalProject/tripMate/web/TripMateController.java
@@ -1,11 +1,17 @@
 package com.trip.finalProject.tripMate.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.trip.finalProject.attachedFile.service.AttachedFileService;
+import com.trip.finalProject.attachedFile.service.AttachedFileVO;
 import com.trip.finalProject.tripMate.service.TripMateService;
 import com.trip.finalProject.tripMate.service.TripMateVO;
 
@@ -14,6 +20,9 @@ public class TripMateController {
 	@Autowired
 	TripMateService tripMateService;
 	
+	@Autowired
+	AttachedFileService attachedFileService;
+	
 	//여행 메이트 글 전체 조회
 	@GetMapping("tripMateList")
 	public String tripMateList(Model model) {
@@ -21,7 +30,7 @@ public class TripMateController {
 		return "tripMate/tripMateList";
 	}
 	
-	//여행 메이트  글 상세 조회
+	//여행 메이트 글 상세 조회
 	@GetMapping("tripMateInfo")
 	public String tripMateInfo(TripMateVO tripMateVO, Model model) {
 		TripMateVO findVO = tripMateService.getTripMateInfo(tripMateVO);
@@ -29,10 +38,31 @@ public class TripMateController {
 		return "tripMate/tripMateInfo";
 	}
 	
+	//여행 메이트 글 등록 - form
+	@GetMapping("tripMateRecruitForm")
+	public String tripMateRecruitForm(Model model) {
+		return "tripMate/tripMateRecruitForm";
+	}
+	
+	//여행 메이트 글 등록 - process
+	@PostMapping("tripMateRecruitInsert")
+	public ModelAndView tripMateRecruitInsert(TripMateVO tripMateVO) {
+		tripMateService.insertTripMateRecruit(tripMateVO);
+		ModelAndView mv = new ModelAndView("redirect:/tripMateList");
+		return mv;
+	}
+	
+	@GetMapping("getAttach")
+	@ResponseBody
+	public List<AttachedFileVO> getAttachList(AttachedFileVO vo){
+		System.out.println(vo.getPostId());
+		return attachedFileService.getAttachList(vo);
+	}
+	
 	//여행 메이트 신청 - form
 	@PostMapping("tripMateApplyForm")
-	public String tripMateForm(Model model) {
-		model.addAttribute("tripMateVO", new TripMateVO());
+	public String tripMateApplyForm(TripMateVO trvo, Model model) {
+		model.addAttribute("tripMateVO", trvo );
 		return "tripMate/tripMateApplyForm";
 	}
 	
