9bc97881cb889f3cbd728fe14134d8c0e59e0dd5
admin <admin@YD02-19>
2023-08-29 15:23:53 +0900

    cartList css 작업, 총 가격 정산 기능 추가, 좋아요 페이지 전체보기(onclick 추가)

--- finalProject/src/main/java/com/trip/finalProject/like/service/LikeService.java
+++ finalProject/src/main/java/com/trip/finalProject/like/service/LikeService.java
@@ -6,19 +6,13 @@ import com.trip.finalProject.common.PagingVO;
 
 public interface LikeService {
 	//패키지 조회(카운트)
-	public int paCountInfo(String memberId);
-	
-	//특산물 조회(카운트)
-	public int spCountInfo(String memberId);
+	public int mtCountInfo(String memberId);
 	
 	//여행기록 조회(카운트)
 	public int trCountInfo(String memberId);
-	
-	//패키지 조회
-	public List<LikeVO> paAllLikeInfo(PagingVO pagingVO);
-	
+
 	//특산물 조회
-	public List<LikeVO> spAllLikeInfo(PagingVO pagingVO);
+	public List<LikeVO> mtAllLikeInfo(PagingVO pagingVO);
 	
 	//여행기록 조회
 	public List<LikeVO> trAllLikeInfo(PagingVO pagingVO);
