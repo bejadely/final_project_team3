8fe7bf29ae9cdda5a9443d71c196a9cc939483ca
admin <admin@YD02-19>
2023-08-29 15:23:53 +0900

    cartList css 작업, 총 가격 정산 기능 추가, 좋아요 페이지 전체보기(onclick 추가)

--- finalProject/src/main/java/com/trip/finalProject/like/web/LikeController.java
+++ finalProject/src/main/java/com/trip/finalProject/like/web/LikeController.java
@@ -20,42 +20,26 @@ public class LikeController {
 	@Autowired
 	LikeService liService;
 	
-	// 패키지 전체 조회
-	@GetMapping("myPaList")
+	// 여행메이트 전체 조회
+	@GetMapping("myMtList")
 	public String paList(Model model, 
 			@RequestParam(value = "nowPage", defaultValue = "1") Integer nowPage,
 			@RequestParam(value = "cntPerPage", defaultValue = "10") Integer cntPerPage) {
 		String memberId = "1";
-		int total = liService.paCountInfo(memberId);
+		int total = liService.mtCountInfo(memberId);
 		System.out.println("토탈 :" + total);
 		PagingVO pagingVO = new PagingVO(total, nowPage, cntPerPage);
 		System.out.println("total: "+pagingVO);
-		List<LikeVO> paList = liService.paAllLikeInfo(pagingVO);
+		List<LikeVO> paList = liService.mtAllLikeInfo(pagingVO);
 
 		model.addAttribute("list", paList);
 		model.addAttribute("paging", pagingVO);
 
-		return "myPage/likeList/paList";
+		return "myPage/likeList/mtList";
 	}
 	
-	// 패키지 전체 조회
-	@GetMapping("mySpList")
-	public String spList(Model model, @RequestParam(value = "nowPage", defaultValue = "1") Integer nowPage,
-			@RequestParam(value = "cntPerPage", defaultValue = "12") Integer cntPerPage) {
-		LikeVO likeVO = new LikeVO();
-		String memberId = likeVO.getMemberId();
-		int total = liService.spCountInfo(memberId);
-		PagingVO pagingVO = new PagingVO(total, nowPage, cntPerPage);
-		List<LikeVO> paList = liService.spAllLikeInfo(pagingVO);
-
-		model.addAttribute("list", paList);
-		model.addAttribute("paging", pagingVO);
-
-		return "myPage/likeList/seList";
-	}
-	
-	// 패키지 전체 조회
-	@GetMapping("myTrList")
+	// 여행계획 전체 조회
+	@GetMapping("/myTrList")
 	public String trList(Model model, @RequestParam(value = "nowPage", defaultValue = "1") Integer nowPage,
 			@RequestParam(value = "cntPerPage", defaultValue = "12") Integer cntPerPage) {
 		LikeVO likeVO = new LikeVO();
@@ -67,6 +51,6 @@ public class LikeController {
 		model.addAttribute("list", paList);
 		model.addAttribute("paging", pagingVO);
 
-		return "trip/tripRecordList";
+		return "myPage/likeList/trList";
 	}
 }
