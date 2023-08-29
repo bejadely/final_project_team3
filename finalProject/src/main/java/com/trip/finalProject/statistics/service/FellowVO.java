891385e8123262e4abdfc2fbac15e1aa263be1f5
fiveglass <ohyulri@gmail.com>
2023-08-30 01:48:17 +0900

    통계 페이지 구현 완료

---
+++ finalProject/src/main/java/com/trip/finalProject/statistics/service/FellowVO.java
@@ -0,0 +1,12 @@
+package com.trip.finalProject.statistics.service;
+
+import lombok.Data;
+
+@Data
+public class FellowVO {
+    private String areaCode;
+    private String sigunguCode;
+    private String fellowType;
+    private String searchNumber;
+    private String yearMonth;
+}
---
+++ finalProject/src/main/java/com/trip/finalProject/statistics/service/RestaurantVO.java
@@ -0,0 +1,13 @@
+package com.trip.finalProject.statistics.service;
+
+import lombok.Data;
+
+@Data
+public class RestaurantVO {
+    private String areaCode;
+    private String sigunguCode;
+    private String restaurantName;
+    private String restaurantType;
+    private String visitorNumber;
+    private String yearMonth;
+}
