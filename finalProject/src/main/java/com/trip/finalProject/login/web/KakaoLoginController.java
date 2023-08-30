d6cb52fa242db89839cac754d90e4bf2da669e79
2eek1 <khwan9404@yu.ac.kr>
2023-08-30 17:49:11 +0900

    카카오 회원가입 db에 넣기

--- finalProject/src/main/java/com/trip/finalProject/login/web/KakaoLoginController.java
+++ finalProject/src/main/java/com/trip/finalProject/login/web/KakaoLoginController.java
@@ -46,7 +46,7 @@ public class KakaoLoginController {
   	session.setAttribute("kakaoN", userInfo.getMemberName());
   	session.setAttribute("kakaoE", userInfo.getEmail());
   	session.setAttribute("kakaoProfile", userInfo.getSavedProfileImg()); 
-  	session.setAttribute("access_Token", access_Token);
+  	session.setAttribute("access_Token", userInfo.getAccessToken());
   	System.out.println("프로필사진:" + userInfo.getSavedProfileImg() );
   	
       
