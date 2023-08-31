11e92ae4d17b01f7ca59c565bf887e478853dd95
LEESEUNGWOO <LEESEUNGWOO@MSDN-1910041457>
2023-09-01 08:05:01 +0900

    0901 home

--- finalProject/src/main/java/com/trip/finalProject/postComment/mapper/PostCommentMapper.java
+++ finalProject/src/main/java/com/trip/finalProject/postComment/mapper/PostCommentMapper.java
@@ -1,8 +1,12 @@
 package com.trip.finalProject.postComment.mapper;
 
+import java.util.List;
+
 import com.trip.finalProject.postComment.service.PostCommentVO;
 
 public interface PostCommentMapper {
+	//해당 게시글의 전체 댓글 조회
+	public List<PostCommentVO> selectAllPostComment();
 	
 	//댓글 저장
 	public int insertPostComment(PostCommentVO postCommentVO);
