0ce09b983c9bbc4a63d9631334a976fc7da2602e
admin <admin@YD02-18>
2023-09-01 11:57:56 +0900

    9/1 오전 머지 전 커밋 - ajax로 댓글 목록 불러오기 구현중

--- finalProject/src/main/java/com/trip/finalProject/postComment/mapper/PostCommentMapper.java
+++ finalProject/src/main/java/com/trip/finalProject/postComment/mapper/PostCommentMapper.java
@@ -6,7 +6,7 @@ import com.trip.finalProject.postComment.service.PostCommentVO;
 
 public interface PostCommentMapper {
 	//해당 게시글의 전체 댓글 조회
-	public List<PostCommentVO> selectAllPostComment();
+	public List<PostCommentVO> selectAllPostComment(String postId);
 	
 	//댓글 저장
 	public int insertPostComment(PostCommentVO postCommentVO);
