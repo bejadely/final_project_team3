bd2f78975d26e488cfae7800311c49afac349bb2
fiveglass <devfiveglass0414@naver.com>
2023-09-04 15:07:52 +0900

    1차 통합테스트 수정 부분 반영 완료, 메인 페이지 구현 중

--- finalProject/src/main/java/com/trip/finalProject/main/service/impl/MainServiceImpl.java
+++ finalProject/src/main/java/com/trip/finalProject/main/service/impl/MainServiceImpl.java
@@ -1,8 +1,18 @@
 package com.trip.finalProject.main.service.impl;
 
 import com.trip.finalProject.main.service.MainService;
+import com.trip.finalProject.main.service.mainSpecialtiesVO;
+
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class MainServiceImpl implements MainService {
+
+	@Override
+	public List<mainSpecialtiesVO> getSpecialtiesInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- finalProject/src/main/java/com/trip/finalProject/main/web/MainController.java
+++ finalProject/src/main/java/com/trip/finalProject/main/web/MainController.java
@@ -1,9 +1,26 @@
 package com.trip.finalProject.main.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.trip.finalProject.main.service.MainService;	
 
 @Controller
 public class MainController {
 
-
+	
+	@Autowired
+	MainService mainservice;
+	
+	
+	@GetMapping("/")
+	public String getSpecialtiesInfo(Model model) {
+		
+		//세일상품 정보 가져오기
+		model.addAttribute("saleItem", mainservice.getSpecialtiesInfo());
+		
+		return "index";
+	}
 }
