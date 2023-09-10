c4c396439b495043f45361126b0829c66ea9aa33
2eek <khwan9404@yu.ac.kr>
2023-09-11 00:34:11 +0900

    공지사항 되살렸음

--- finalProject/src/main/java/com/trip/finalProject/notice/service/Impl/NoticeServiceImpl.java
+++ finalProject/src/main/java/com/trip/finalProject/notice/service/Impl/NoticeServiceImpl.java
@@ -99,29 +99,28 @@ public class NoticeServiceImpl implements NoticeService {
 		}
 	}
 	//게시물 타입이 공지사항(n1)인 게시글 갯수 카운트
-	public int countNoticeBySearch(NoticeVO vo) {
-		return noticeMapper.countNoticeBySearch(vo); 	
+	public int countNoticeType1n() {
+		return noticeMapper.countNoticeType1n(); 	
 	}
 	
-	/*
-	 * //게시물 타입이 공지사항(n2)인 게시글 갯수 카운트 public int countNoticeType2n() { return
-	 * noticeMapper.countNoticeType2n(); }
-	 */
+	//게시물 타입이 공지사항(n2)인 게시글 갯수 카운트
+		public int countNoticeType2n() {
+			return noticeMapper.countNoticeType2n(); 	
+		}
 	
 	
 	
 	@Override
-	public List<NoticeVO> searchNoticeByTitle(NoticeVO VO, PagingVO pagingVO) {
+	public List<NoticeVO> searchNoticeByTitle1n(NoticeVO noticeVO, PagingVO pagingVO) {
 		// 공지사항인 글을 제목으로 검색
-		System.out.println("노티스있냐"+VO);
-		return noticeMapper.searchByNoticeByTitle(VO, pagingVO);
+		return noticeMapper.searchByNoticeByTitle1n(noticeVO, pagingVO);
 	} 
 	
-	/*
-	 * @Override public List<NoticeVO> searchNoticeByTitle2n(NoticeVO noticeVO,
-	 * PagingVO pagingVO) { // 이벤트인 글을 제목으로 검색 return
-	 * noticeMapper.searchByNoticeByTitle2n(noticeVO, pagingVO); }
-	 */
+	@Override
+	public List<NoticeVO> searchNoticeByTitle2n(NoticeVO noticeVO, PagingVO pagingVO) {
+		// 이벤트인 글을 제목으로 검색
+		return noticeMapper.searchByNoticeByTitle2n(noticeVO, pagingVO);
+	} 
 	
 	@Override
 	public int boardUpdate(NoticeVO vo) {
