3c61a41968f0369566a03c1d9a2b8dabd61a5dd1
jkeb1 <jkeb1@jkeb196>
2023-09-12 06:47:24 +0900

    스케줄러

--- finalProject/src/main/java/com/trip/finalProject/packaged/service/impl/PackageQuartzJob.java
+++ finalProject/src/main/java/com/trip/finalProject/packaged/service/impl/PackageQuartzJob.java
@@ -1,5 +1,8 @@
 package com.trip.finalProject.packaged.service.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,27 +10,38 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.springframework.stereotype.Service;
 
 import com.trip.finalProject.packaged.mapper.PackageMapper;
+import com.trip.finalProject.packaged.service.PackageVO;
 
 @Service
 public class PackageQuartzJob extends QuartzJobBean{
 	@Autowired
 	PackageMapper packageMapper;
 	
-	@Override
-	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-		
-		try {
-			int result = packageMapper.packageCalculate();
-			
-			result = packageMapper.packageUpdateDeadline();
-			
-			if(result > 0) {
-				
-				System.out.println("여행기간이 지난 여행기록의 여행완료 처리되었습니다.");
-			}		
-					
-		}catch (Exception e) {
-			System.out.println(e);
-		}
+	 @Override
+	    protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+
+	        try {
+	            // 스케줄러 코드 작성
+	        	
+
+	            List<PackageVO> dataList = packageMapper.getPackageCalculate();
+	            // 데이터를 저장할 리스트 생성
+	            List<PackageVO> dataToInsert = new ArrayList<>();
+
+	            // 데이터를 리스트에 저장
+	            for (PackageVO data : dataList) {
+	                // 필요한 가공 또는 조작을 수행하고 리스트에 추가
+	                dataToInsert.add(data);
+	            }
+	            
+	            for (PackageVO data : dataToInsert) {
+	                packageMapper.packageCalculate(data);
+	            }
+
+	            packageMapper.packageUpdateDeadline();
+	        } catch (Exception e) {
+	            System.out.println("스케줄러 작업 중 오류 발생: " + e.getMessage());
+	        }
+	    }
+	
 	}
-}
